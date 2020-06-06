9cc2d2ecb1b1ce6ae203deb79969764503701cc8
Anton Podolsky <anton.podolsky@gmail.com>
2020-06-06 16:09:43 +0300

    change min image size

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const StorageKey = 'exifysettings';
 export const OverlayHeight = 60;
 export const OverlayCompactHeight = 40;
-export const MinLongSideLength = 600;
+export const MinLongSideLength = 500;
 
 export enum DefaultExifProperties {
   Model = 'Equipment',
